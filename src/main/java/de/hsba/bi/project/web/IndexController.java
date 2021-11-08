f324a83a35cd6ef816b280c55a6b2afa09fd7da1
Jessicadittrichnaja <93939006+Jessicadittrichnaja@users.noreply.github.com>
2021-11-08 20:12:42 +0100

    k,k

--- src/main/java/de/hsba/bi/project/web/IndexController.java
+++ src/main/java/de/hsba/bi/project/web/IndexController.java
@@ -1,11 +1,17 @@
 package de.hsba.bi.project.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String index(Model model, @RequestParam(name= "hello", required= false)String hello) {
+        if(hello!= null) {
+            model.addAttribute("greeting", "Hello, " + hello+ "!");
+        }
+        return"index";
     }
 }
