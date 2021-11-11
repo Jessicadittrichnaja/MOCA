1c7960eafc156bfab45743947982298fae933c73
Jessicadittrichnaja <93939006+Jessicadittrichnaja@users.noreply.github.com>
2021-11-11 22:35:14 +0100

    HTML-Seiten angelegt und mit Mapping verlinkt. Header, Footer und main.css erstellt. Formular mit Controller für Veranstaltungsübersicht angefangen.

--- src/main/java/de/hsba/bi/project/web/CourseController.java
+++ src/main/java/de/hsba/bi/project/web/CourseController.java
@@ -1,4 +1,25 @@
 package de.hsba.bi.project.web;
 
+import de.hsba.bi.project.form.form;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class CourseController {
+
+    @GetMapping("/overview")
+    public String Form(Model model) {
+        model.addAttribute("form", new form());
+        return "overview";
+    }
+
+    @PostMapping("/overview")
+    public String formSubmit(@ModelAttribute form form, Model model) {
+        model.addAttribute("form", form);
+        return "result";
+    }
+
 }
--- src/main/java/de/hsba/bi/project/web/IndexController.java
+++ src/main/java/de/hsba/bi/project/web/IndexController.java
@@ -1,10 +1,45 @@
 package de.hsba.bi.project.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class IndexController {
-    @RequestMapping(path = "/Overview")
-    public String getGreeting() {
-        return "Overview";
+
+
+    @RequestMapping("/index")
+    public String index(Model model) {
+
+        return "index";
     }
+    @RequestMapping("/overview")
+    public String overview(Model model) {
+
+        return "overview";
+    }
+    @RequestMapping("/login")
+    public String login(Model model) {
+
+        return "login";
+    }
+    @RequestMapping("/impressum")
+    public String impressum(Model model) {
+
+        return "impressum";
+    }
+
+    @RequestMapping("/kontakt")
+    public String kontakt(Model model) {
+
+        return "kontakt";
+    }
+    @RequestMapping("/datenschutzerklärung")
+    public String datenschutzerklaerung(Model model) {
+
+        return "datenschutzerklärung";
+    }
+
 }
+
+
