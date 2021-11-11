0b105b2096fc246c2cc040f136125fc58696989a
Jessicadittrichnaja <93939006+Jessicadittrichnaja@users.noreply.github.com>
2021-11-11 22:37:21 +0100

    HTML-Seiten angelegt und mit Mapping verlinkt. Header, Footer und main.css erstellt. Formular mit Controller für Veranstaltungsübersicht angefangen.

---
+++ src/main/java/de/hsba/bi/project/form/form.java
@@ -0,0 +1,35 @@
+package de.hsba.bi.project.form;
+
+import java.util.Date;
+
+public class form {
+
+    private String title;
+    private String category;
+    private Date date;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
