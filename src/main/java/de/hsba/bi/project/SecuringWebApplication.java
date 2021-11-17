82de205af71e41e4a634664b39e1d355dd63138b
Enrico0123 <93938994+Enrico0123@users.noreply.github.com>
2021-11-17 22:44:13 +0100

    22:43 - 17.11.2021

---
+++ src/main/java/de/hsba/bi/project/SecuringWebApplication.java
@@ -0,0 +1,13 @@
+package de.hsba.bi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SecuringWebApplication.class, args);
+	}
+
+}
