a9c1ecabe34ff99fd6f991f045f37562818516c1
fynn77 <93922298+fynn77@users.noreply.github.com>
2021-12-01 19:32:02 +0100

    security
    .

--- src/main/java/de/hsba/bi/project/web/IndexController.java
+++ src/main/java/de/hsba/bi/project/web/IndexController.java
@@ -1,8 +1,13 @@
 package de.hsba.bi.project.web;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
 
 @Controller
 public class IndexController {
@@ -49,6 +54,12 @@ public class IndexController {
     public String createEvent(Model model) {
 
         return "createEvent";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth instanceof AnonymousAuthenticationToken ? "login" : "redirect:/";
     }
 
 }
