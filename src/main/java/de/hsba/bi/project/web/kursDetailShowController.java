8bd7df2b8495c41f76a41b4c488e216f40ebc3b2
Daniel <daniel.fuhrmann@live.de>
2021-12-18 11:42:49 +0100

    18.12.2021
    .

---
+++ src/main/java/de/hsba/bi/project/web/kursDetailShowController.java
@@ -0,0 +1,28 @@
+package de.hsba.bi.project.web;
+
+import de.hsba.bi.project.events.EventRepository;
+import de.hsba.bi.project.events.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class kursDetailShowController {
+
+    @Autowired
+    private EventRepository eventRepository;
+    @Autowired
+    private EventService eventService;
+
+    //
+
+    @GetMapping("/kursDetails/{id}")
+    public String showChosenEvent(@PathVariable("id") int id, Model model) {
+        model.addAttribute("events", eventService.findById(id));
+        return "kursDetails";
+    }
+}
