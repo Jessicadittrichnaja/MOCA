bd7d4da2b93bc3d0f36c5ca365f58be278b4d211
Daniel <daniel.fuhrmann@live.de>
2021-12-29 12:43:36 +0100

    29.12.2021
    .
    Layout/Styling & TestData

--- src/main/java/de/hsba/bi/project/events/TestData.java
+++ src/main/java/de/hsba/bi/project/events/TestData.java
@@ -24,12 +24,12 @@ public class TestData {
         @Transactional
         public void init() {
             // add some events
-            Event programmierung = new Event ("Programmierung 2", "Projekt", Category.Seminar, 10, 1, Location.Hamburg, LocalDate.of(2021, 10, 23), LocalTime.of(10, 15));
-            Event stadttour = new Event("Stadttour", "Freizeitveranstaltung", Category.Teamtag, 3, 15, Location.Berlin, LocalDate.of(2021, 8, 20), LocalTime.of(11, 30));
-            Event agileMethoden = new Event("Agile Methoden", "In diesem Seminar lernen Sie alles zum Thema agile Methoden", Category.Seminar, 3, 15, Location.München, LocalDate.of(2021, 12, 8), LocalTime.of(15, 45));
-            Event test1 = new Event ("Test", "Test 1", Category.Seminar, 10, 1, Location.Hamburg, LocalDate.of(2021, 10, 23), LocalTime.of(9, 00));
-            Event test2 = new Event("Test Test", "Test 2", Category.Teamtag, 3, 15, Location.Berlin, LocalDate.of(2021, 8, 20), LocalTime.of(10, 00));
-            Event test3 = new Event("Test Test Test", "Test 3", Category.Seminar, 3, 15, Location.München, LocalDate.of(2021, 12, 8), LocalTime.of(18, 00));
+            Event programmierung = new Event ("Programmierung 2", "Ziel des Kurses Programmierung 2 ist das Erlernen grundlegender Konzepte der imperativen und objektorientierten Programmierung anhand der Programmiersprache Java.", Category.Seminar, 10, 1, Location.Hamburg, LocalDate.of(2021, 10, 23), LocalTime.of(10, 15));
+            Event stadttour = new Event("Stadtrundfahrt", "Begeben Sie sich auf eine Stadtrundfahrt durch Berlin - mit Bus, Schiff, auf dem Fahrrad oder zu Fuß. Vorbei an den klassischen Sehenswürdigkeiten wie das Brandenburger Tor, das Reichstagsgebäude und der Checkpoint Charlie", Category.Teamtag, 3, 15, Location.Berlin, LocalDate.of(2021, 8, 20), LocalTime.of(11, 30));
+            Event agileMethoden = new Event("Agile Methoden", "In diesem Seminar lernen Sie alles zum Thema agile Methoden.", Category.Seminar, 3, 15, Location.München, LocalDate.of(2021, 12, 8), LocalTime.of(15, 45));
+            Event test1 = new Event ("Excel Kurs (Basiswissen)", "Der Umgang mit Microsoft Excel ist mittlerweile zu einer Grundvoraussetzung im Büroalltag geworden. Das Basiswissen und die wichtigsten Funktionalitäten von Microsoft Excel werden in diesem Kurs vermittelt.", Category.Seminar, 4, 15, Location.Hamburg, LocalDate.of(2021, 10, 23), LocalTime.of(9, 00));
+            Event test2 = new Event("Excel Kurs (Advanced)", "Dieser Kurs deckt einen großen Teil des benötigten „Standard“ Excel Wissens für Angestellte ab. Der Kurs ist für Mitarbeiter, welche bereits viel Kontakt mit Excel haben und nun Ihr vorhandenes Wissen weiter vertiefen wollen.", Category.Teamtag, 8, 10, Location.Berlin, LocalDate.of(2021, 8, 20), LocalTime.of(10, 00));
+            Event test3 = new Event("Buchführung und Bilanzierung", "Durch das Seminar „Buchführung und Bilanzierung“ werden Sie in der Lage sein, Buchführungsaufgaben nach HGB in Ihrem Unternehmen oder für Ihre Kunden sicher, vollständig und korrekt auszuüben.", Category.Seminar, 3, 15, Location.München, LocalDate.of(2021, 12, 8), LocalTime.of(18, 00));
             eventRepository.save(programmierung);
             eventRepository.save(stadttour);
             eventRepository.save(agileMethoden);
