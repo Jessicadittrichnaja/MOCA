9d7fff80e276e0497f1c8c86342e69908e6ffb93
Jessicadittrichnaja <93939006+Jessicadittrichnaja@users.noreply.github.com>
2022-01-03 15:01:49 +0100

    .

---
+++ src/main/java/de/hsba/bi/project/events/headSeminar.java
@@ -0,0 +1,17 @@
+package de.hsba.bi.project.events;
+
+// Ermöglicht Auswahl verschiedener Standorte beim Erstellen/ Ändern von Events
+
+public enum headSeminar {
+    Klausi("Klausi Peter"), Günther("Günther Müller"), Maxi("Maxi Mustermann"), Anne("Anne Susanne"), Sabine("Sabine Richter");
+
+    private final String displayValue;
+
+    private headSeminar(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
+}
