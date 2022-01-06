844d1dad638586650e251d79ea4f01b1a3b6cb77
Daniel <daniel.fuhrmann@live.de>
2022-01-06 22:27:54 +0100

    Test

--- src/test/java/de/hsba/bi/project/filterProcess/FilterTest.java
+++ src/test/java/de/hsba/bi/project/filterProcess/FilterTest.java
@@ -1,11 +1,9 @@
 package de.hsba.bi.project.filterProcess;
 
-import de.hsba.bi.project.events.Category;
-import de.hsba.bi.project.events.Daytime;
-import de.hsba.bi.project.events.Event;
-import de.hsba.bi.project.events.Location;
+import de.hsba.bi.project.events.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -14,44 +12,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FilterTest {
 
-    String              name;
-    String              description;
-    Category            selectedCategory;
-    Integer             duration;
-    Integer             spots;
-    Location            selectedLocation;
-    LocalDate           selectedDate;
-    LocalTime           startTime;
-    LocalTime           endTime;
+    String name;
+    String description;
+    de.hsba.bi.project.events.headSeminar headSeminar;
+    Category selectedCategory;
+    Integer duration;
+    Integer spots;
+    Location selectedLocation;
+    LocalDate selectedDate;
+    LocalTime startTime;
+    LocalTime endTime;
 
+    @Autowired
+    EventRepository underTest;
 
     @Test
-    @DisplayName("Filtering Events should work")
-    void testFilterEvents() {
+    @DisplayName("Creating a new Event")
+    void testCreateEvent() {
+
         // given
-        name                = new String("Programmierung 2");
-        description         = new String("Programmierveranstaltung");
-        selectedCategory    = Category.Seminar;
-        duration            = 3;
-        spots               = 1;
-        selectedLocation    = Location.Seminarraum1;
-        selectedDate        = LocalDate.of(2021, 10, 23);
-        startTime           = LocalTime.of(15, 30);
-        endTime             = LocalTime.of(15,30).plusHours(duration);
+        name = new String("Weinverkostung");
+        description = new String("Stellen Sie ihre Weinkenntnisse unter Beweis in diesem lustigen Workshop. Probieren Sie Wein aus aller Welt und finden Sie Ihren neuen Favoriten.");
+        headSeminar = headSeminar.Günther;
+        selectedCategory = Category.Seminar;
+        duration = 2;
+        spots = 10;
+        selectedLocation = Location.Seminarraum1;
+        selectedDate = LocalDate.of(2022, 11, 21);
+        startTime = LocalTime.of(15, 30);
+        endTime = LocalTime.of(15, 30).plusHours(2);
 
         // when
-        Event event = new Event(name, description, selectedCategory, duration, spots, selectedLocation, selectedDate, startTime,endTime);
+        Event event = new Event(name, description, headSeminar, selectedCategory, duration, spots, selectedLocation, selectedDate, startTime, endTime);
 
         // then
-        String name                 = "Programmierung 2";
-        String description          = "Programmierveranstaltung";
-        String selectedCategory     = "Seminar";
-        Integer duration            =  3;
-        Integer spots               =  1;
-        String selectedLocation     = "Seminarraum1";
-        LocalDate selectedDate      = LocalDate.of(2021,10,23);
-        LocalTime startTime         = LocalTime.of(15,30);
-        LocalTime endTime           = LocalTime.of(15,30).plusHours(duration);
+        String name = "Weinverkostung";
+        String description = "Stellen Sie ihre Weinkenntnisse unter Beweis in diesem lustigen Workshop. Probieren Sie Wein aus aller Welt und finden Sie Ihren neuen Favoriten.";
+        String headSeminar = "Günther";
+        String selectedCategory = "Seminar";
+        Integer duration = 2;
+        Integer spots = 10;
+        String selectedLocation = "Seminarraum1";
+        LocalDate selectedDate = LocalDate.of(2022, 11, 21);
+        LocalTime startTime = LocalTime.of(15, 30);
+        LocalTime endTime = LocalTime.of(15, 30).plusHours(duration);
 
         assertThat(event.getCategory().toString()).isEqualTo(selectedCategory);
         assertThat(event.getName()).isEqualTo(name);
@@ -62,6 +66,5 @@ public class FilterTest {
         assertThat(event.getDate()).isEqualTo(selectedDate);
         assertThat(event.getStartTime()).isEqualTo(startTime);
         assertThat(event.getEndTime()).isEqualTo(endTime);
-
     }
 }
